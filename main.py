2998dd35f1997ac226acfe9feee25fbb5a74c0f5
Sandeep <talapally.kumar@oracle.com>
2024-11-08 23:30:57 +0530

    aded folder

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI, Header, HTTPException, Request
 import os
 from fastapi.responses import JSONResponse
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -33,6 +34,15 @@ async def webhook(request: Request, x_hub_signature: str = Header(None)):
 @app.get("/")
 async def root():
     return {"message": "Hello, Vercel!"}
+
+@app.get("/privacy-policy", response_class=HTMLResponse)
+async def privacy_policy():
+    # Read the privacy_policy.html file
+    file_path = os.path.join(os.path.dirname(__file__), "privacy_policy.html")
+    with open(file_path, "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content)
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
